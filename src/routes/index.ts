cd933850e979fb35e27810af468afa0cb449f477
abgq <berkay.guezel@rwth-aachen.de>
2025-02-02 23:33:42 +0100

    NaN

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
-import { Router } from 'express';
+import { Router, Express } from 'express';
 import MealController from '../controllers';
 
 const router = Router();
 const mealController = new MealController();
 
-export function setRoutes(app) {
+export function setRoutes(app: Express) {
     app.use('/api/meals', router);
     router.get('/', mealController.getMeals.bind(mealController));
     router.post('/ingredients', mealController.addIngredient.bind(mealController));
